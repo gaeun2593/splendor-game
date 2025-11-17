9c635368f113f1c01a185ad48cde5d293a8dfa90
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-11-18 01:49:37 +0900

    feat(domain): StaticCard 생성자 수정

--- src/main/java/com/splendor/project/domain/data/entity/StaticCard.java
+++ src/main/java/com/splendor/project/domain/data/entity/StaticCard.java
@@ -3,11 +3,13 @@ package com.splendor.project.domain.data.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "static_cards")
 public class StaticCard {
