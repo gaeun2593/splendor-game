2f8ea5f6d60a124a28afe0241c3403388d37f7db
gaeun2593@gmail.com <gaeun2593@gmail.com>
2025-11-20 22:57:11 +0900

    feat: GameState 레포지토리 추가

--- src/main/java/com/splendor/project/domain/game/repository/GameStateRepository.java
+++ src/main/java/com/splendor/project/domain/game/repository/GameStateRepository.java
@@ -2,11 +2,9 @@ package com.splendor.project.domain.game.repository;
 
 import com.splendor.project.domain.game.dto.response.GameStateDto;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface GameStateRepository extends CrudRepository<GameStateDto, Long> {
     Optional<GameStateDto> findById(Long gameId);
 }
